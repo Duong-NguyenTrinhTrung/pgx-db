073a796973776fd5f363b6f74b4289428bae34aa
SaadHassanKhan <saadhassan909@gmail.com>
2023-12-18 17:25:04 +0500

    Update drug_network.js

--- static/drug/js/drug_network.js
+++ static/drug/js/drug_network.js
@@ -1548,7 +1548,7 @@ var simulation = null
 // Create the Forced Directed Network Chart
 function createChart(links) {
     d3.select("#chart").selectAll("*").remove();
-    console.log("Latest Edit CreateCHart_18_12_E");
+    console.log("Latest Edit CreateCHart_18_12_F");
     var container = d3.select("#chart");
     //debugger
      var containerWidth = [container.node().getBoundingClientRect().width] - 10;
