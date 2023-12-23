ca8b502914639729fe31abdc9a0c4913999771cf
nguyentrinhtrungduong <nguyentrinhtrungduong@gmail.com>
2023-12-23 10:30:31 +0100

    Adding about page

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,14 @@
 # from django.conf.urls import url
 from django.urls import path
 from django.views.decorators.cache import cache_page
-from .views import Home, drug_target_network, drug_lookup, target_lookup, drug_autocomplete_view, protein_autocomplete_view, variant_autocomplete_view, variant_lookup, variant_statistics, target_statistics
+from .views import Home, drug_target_network, drug_lookup, target_lookup, drug_autocomplete_view, protein_autocomplete_view, variant_autocomplete_view, variant_lookup, variant_statistics, target_statistics, about_pgx
 
 
 urlpatterns = [
     path("", Home.as_view(), name='home'),
     path("drug_target_network/", drug_target_network, name='drug_target_network'),
     path("drug_lookup/", drug_lookup, name='drug_lookup'),
+    path("about_pgx/", about_pgx, name='about-pgx'),
     path("target_lookup/", target_lookup, name='target_lookup'),
     path("variant_lookup/", variant_lookup, name='variant_lookup'),
     path("variant_statistics/", variant_statistics, name='variant_statistics'),
