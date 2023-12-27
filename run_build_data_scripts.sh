0b53da0c7f8238ec911a6c2a6e42b17719575c13
nguyentrinhtrungduong <nguyentrinhtrungduong@gmail.com>
2023-12-27 12:22:59 +0100

    Adding long time local memmory cache

---
+++ run_build_data_scripts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Start run build data scripts" 
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Run build data scripts
+
+echo "Start run many builds" 
+
+#python manage.py build_atc_anatomic_group
+python scripts/run_many_builds.py
+
+echo "End run many builds"
+
+# Run other scripts
+
+###
+
+echo "End run build data scripts"
+
+# Deactivate virtual environment
+deactivate
