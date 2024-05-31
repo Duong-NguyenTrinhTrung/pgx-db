1f06ed6e68a2de9df09bd4d913b620b3f01d9a8e
nguyentrinhtrungduong <nguyentrinhtrungduong@gmail.com>
2024-05-31 15:50:08 +0200

    Fix AA index in 3D view

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 # from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from django.views.decorators.cache import cache_page
 from .views import Home, drug_target_network, drug_lookup, target_lookup, drug_autocomplete_view, protein_autocomplete_view, variant_autocomplete_view, variant_lookup, chromosome_mapper, variant_mapper, target_statistics, about_pgx, get_chromosome_mapping, get_variant_mapping, get_chromosome_mapping_example, get_variant_mapping_example, anno_from_autocomplete_view, variant_anno_from_autocomplete_view, disease_lookup, disease_statistics, disease_autocomplete_view, contribute_to_pgx, tutorial, get_se_definition
 
