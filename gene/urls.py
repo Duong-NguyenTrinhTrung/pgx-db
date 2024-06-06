42a3b0ddd5666643768ad50da305de5dc096a7fe
nguyentrinhtrungduong <nguyentrinhtrungduong@gmail.com>
2024-06-06 16:46:24 +0200

    Improve structure and loading speed of gene detail page

--- gene/urls.py
+++ gene/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, re_path
 from .views import (
-    GeneDetailBrowser,
     GenebassVariantListView,
     filter_gene_detail_page,
+    get_gene_detail_data,
+    get_variant_annotation_and_vep
 )
 
 app_name = 'gene' # to distingush different app names when using url names
 
 urlpatterns = [
-    path('gene/<slug:slug>/', GeneDetailBrowser.as_view(), name='gene'),
-    path('gene/<str:id>/filter', filter_gene_detail_page, name='filter-gene-detail-page'),
-    path('gene/<str:pk>/genebass_variants/', GenebassVariantListView.as_view(), name='genebass-variants'),
+    path('gene/gene_detail/<slug:slug>/', get_gene_detail_data, name='get-gene-detail-data'),
+    path('gene/vaav/<slug:slug>/', get_variant_annotation_and_vep, name='get-variant-annotation-and-vep'),
     
 ]
