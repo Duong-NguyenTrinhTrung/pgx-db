9b6f47f99ce0cb2225b1663593c55060586598fe
nguyentrinhtrungduong <nguyentrinhtrungduong@gmail.com>
2024-06-24 18:30:33 +0200

    Adjust settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -149,7 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "static", "json-sample")
 )
 
 # Default primary key field type
